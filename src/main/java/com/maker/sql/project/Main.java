a60390eb0da940db5b33e268d7c07576471d6623
DiegoAndradeD <diegoadeiro313@gmail.com>
2024-06-03 20:12:21 -0300

    Isolar responsabilidades de switch do menu da classe main Closes #17

--- src/main/java/com/maker/sql/project/Main.java
+++ src/main/java/com/maker/sql/project/Main.java
@@ -7,44 +7,12 @@ import com.maker.sql.project.services.EntityServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        EntityService entityService = new EntityServiceImpl();
-        Menu menu = new Menu(scanner, entityService);
-
-        while (true) {
-            menu.displayMainMenu();
-            int option = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (option) {
-                case 1:
-                    menu.createEntity();
-                    break;
-                case 2:
-                    menu.listEntities();
-                    break;
-                case 3:
-                    menu.generateSelectAllSql();
-                    break;
-                case 4:
-                    menu.generateSelectSql();
-                    break;
-                case 5:
-                    menu.generateInsertSql();
-                    break;
-                case 6:
-                    menu.generateDeleteSql();
-                    break;
-                case 7:
-                    menu.generateUpdateSql();
-                    break;
-                case 0:
-                    System.out.println("Encerrando o programa...");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
+        try (Scanner scanner = new Scanner(System.in)) {
+            EntityService entityService = new EntityServiceImpl();
+            Menu menu = new Menu(scanner, entityService);
+            menu.run();
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
         }
     }
 }
